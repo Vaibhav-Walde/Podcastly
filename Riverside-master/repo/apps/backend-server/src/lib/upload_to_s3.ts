3cbba139cb7ca3b9fa06aab18d348c6d561e0d83
Vaibhav-Walde <vaibhavwalde99@gmail.com>
2026-03-29 10:09:08 +0530

    feat: Supabase Storage for recordings

--- Riverside-master/repo/apps/backend-server/src/lib/upload_to_s3.ts
+++ Riverside-master/repo/apps/backend-server/src/lib/upload_to_s3.ts
@@ -1,21 +1,26 @@
 import { fileType } from "../types/types";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
-import { s3 } from "../clients/S3_client";
-import dotenv from "dotenv"
-dotenv.config();
+import { createClient } from "@supabase/supabase-js";
 
-// Function to Upload Files to S3
-export async function uploadToS3(file:fileType){
-    console.log(file);
-    const uploadCommand = new PutObjectCommand({
-        Bucket:process.env.S3_BUCKET_NAME!,
-        Key:file.originalname,
-        Body:file.buffer,
-        ContentType:file.mimetype
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_KEY!
+);
+
+export async function uploadToS3(file: fileType) {
+  console.log('Uploading to Supabase:', file.originalname);
+  
+  const { error } = await supabase.storage
+    .from('recordings')
+    .upload(file.originalname, file.buffer, {
+      contentType: file.mimetype,
+      upsert: true
     });
-    await s3.send(uploadCommand)
 
-    const s3Url = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${file.originalname}`
-    console.log(s3Url)
-    return s3Url;
+  if (error) throw error;
+
+  const { data } = supabase.storage
+    .from('recordings')
+    .getPublicUrl(file.originalname);
+
+  return data.publicUrl;
 }
