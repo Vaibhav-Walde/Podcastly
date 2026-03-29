633a5f56fc458a108396d98ba08d4b2d4403f739
Vaibhav-Walde <vaibhavwalde99@gmail.com>
2026-03-29 10:17:23 +0530

    fix: Redis connection use env URL

--- Riverside-master/repo/apps/backend-server/src/lib/redis_connection.ts
+++ Riverside-master/repo/apps/backend-server/src/lib/redis_connection.ts
@@ -1,3 +1,5 @@
-import IORedis  from "ioredis"
-
-export const connection = new IORedis({maxRetriesPerRequest:null});
+import IORedis from "ioredis"
+export const connection = new IORedis(process.env.REDIS_URL || "redis://localhost:6379", {
+  maxRetriesPerRequest: null,
+  tls: process.env.REDIS_URL?.startsWith('rediss://') ? {} : undefined
+});
